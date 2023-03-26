174e8752347be455f5b616baa62a731047c227a0
dpp17 <durga123q.com>
2023-03-26 13:20:33 +0530

    Added:: EmployeePayroll with local storage

---
+++ UC3_PayrollObject/js/payroll_form.js
@@ -0,0 +1,36 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error')
+    name.addEventListener('input', function () {
+        if (name.value.length == 0) {
+            textError.textContent = '';
+            return;
+        }
+
+        try {
+            (new EmployeePayrollData()).name = name.value;
+            textError.textContent = '';
+        } catch (e) {
+            textError.textContent = e;
+        }
+    })
+    function getSalary() {
+        output.textContent = salary.value;
+    }
+
+    const salary = document.querySelector('#salary')
+    const output = document.querySelector('.salary-output')
+    output.textContent = salary.value
+    salary.addEventListener('input', getSalary)
+
+});
+
+// day 44 uc 3
+const save = () => {
+    try {
+        let employeePayrollData = createEmployeePayroll();
+        createAndUpdateStorage(employeePayrollData);
+    } catch (e) {
+        return;
+    }
+}
